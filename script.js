66ab4064ac0e8ea403f128938f6275ef7d1ffa89
prathamesh093 <parthp4018@gmail.com>
2023-12-04 20:20:30 +0530

    new changes

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
-function maskPassword(pass){
-    let str = ""
+function maskPassword(pass) {
+    let str = "";
     for (let index = 0; index < pass.length; index++) {
-        str  += "*"
+        str += "*";
     }
-    return str
+    return str;
 }
 
 function copyText(txt) {
     navigator.clipboard.writeText(txt).then(
         () => {
-          /* clipboard successfully set */
-          document.getElementById("alert").style.display = "inline"
-          setTimeout(() => {
-            document.getElementById("alert").style.display = "none"
-          }, 2000);
-
+            /* clipboard successfully set */
+            document.getElementById("alert").style.display = "inline";
+            setTimeout(() => {
+                document.getElementById("alert").style.display = "none";
+            }, 2000);
         },
         () => {
-          /* clipboard write failed */
-          alert("Clipboard copying failed")
+            /* clipboard write failed */
+            alert("Clipboard copying failed");
         },
-      );
-  }
-
-const deletePassword = (website)=>{
-    let data = localStorage.getItem("passwords")
-    let arr = JSON.parse(data);
-    arrUpdated = arr.filter((e)=>{
-        return e.website != website
-    })
-    localStorage.setItem("passwords", JSON.stringify(arrUpdated))
-    alert(`Successfully deleted ${website}'s password`)
-    showPasswords()
-
+    );
 }
+
+const deletePassword = (website) => {
+    let data = localStorage.getItem("passwords");
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e) => {
+        return e.website != website;
+    });
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+    alert(`Successfully deleted ${website}'s password`);
+    showPasswords();
+};
 
 // Logic to fill the table
 const showPasswords = () => {
-    let tb = document.querySelector("table")
-    let data = localStorage.getItem("passwords")
+    let tb = document.querySelector("table");
+    let data = localStorage.getItem("passwords");
     if (data == null || JSON.parse(data).length == 0) {
-        tb.innerHTML = "No Data To Show"
-    }
-    else {
-        tb.innerHTML =  `<tr>
+        tb.innerHTML = "No Data To Show";
+    } else {
+        tb.innerHTML = `<tr>
         <th>Website</th>
         <th>Username</th>
         <th>Password</th>
         <th>Delete</th>
-    </tr> `
+    </tr> `;
         let arr = JSON.parse(data);
-        let str = ""
+        let str = "";
         for (let index = 0; index < arr.length; index++) {
             const element = arr[index];
 
@@ -62,35 +59,42 @@ const showPasswords = () => {
     <td>${maskPassword(element.password)} <img onclick="copyText('${element.password}')" src="./copy.svg" alt="Copy Button" width="10" width="10" height="10">
     </td>
     <td><button class="btnsm" onclick="deletePassword('${element.website}')">Delete</button></td>
-        </tr>`
+        </tr>`;
         }
-        tb.innerHTML = tb.innerHTML + str
-
+        tb.innerHTML = tb.innerHTML + str;
     }
-    website.value = ""
-    username.value = ""
-    password.value = ""
-}
+    website.value = "";
+    username.value = "";
+    password.value = "";
+};
 
 console.log("Working");
-showPasswords()
+showPasswords();
+
 document.querySelector(".btn").addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log("Clicked....")
-    console.log(username.value, password.value)
-    let passwords = localStorage.getItem("passwords")
-    console.log(passwords)
+    e.preventDefault();
+
+    // Input validation
+    if (!website.value || !username.value || !password.value) {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    console.log("Clicked....");
+    console.log(username.value, password.value);
+
+    let passwords = localStorage.getItem("passwords");
+    console.log(passwords);
     if (passwords == null) {
-        let json = []
-        json.push({website: website.value, username: username.value, password: password.value })
+        let json = [];
+        json.push({ website: website.value, username: username.value, password: password.value });
         alert("Password Saved");
-        localStorage.setItem("passwords", JSON.stringify(json))
-    }
-    else {
-        let json = JSON.parse(localStorage.getItem("passwords"))
-        json.push({ website: website.value, username: username.value, password: password.value })
+        localStorage.setItem("passwords", JSON.stringify(json));
+    } else {
+        let json = JSON.parse(localStorage.getItem("passwords"));
+        json.push({ website: website.value, username: username.value, password: password.value });
         alert("Password Saved");
-        localStorage.setItem("passwords", JSON.stringify(json))
+        localStorage.setItem("passwords", JSON.stringify(json));
     }
-    showPasswords()
-})
+    showPasswords();
+});
